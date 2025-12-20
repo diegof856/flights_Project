d6710b40e2c76de6a844251c96340546d0f6f38f
Diego <diegofelipe1025@gmail.com>
2025-12-20 12:12:46 -0300

    feat:adding navbar to the front-end

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 //pages
 import Home from './pages/Home'
+//components
+import NavBar from './components/NavBar'
 function App() {
  
 
   return (
     <>
+   
       <BrowserRouter>
+       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
--- front_end/src/components/NavBar.tsx
+++ front_end/src/components/NavBar.tsx
@@ -1,7 +1,12 @@
+import style from "./NavBar.module.css";
 const NavBar = () => {
   return (
-    <nav>
-    
+    <nav className="sm md lg">
+      <div className={style.nav_images}>
+      <img src="/logo.svg" alt="Logotipo da pagina" />
+      <img src="/text.svg" alt="Texto com o nome da pilops" />
+      </div>
+      <p>Your virtual pilot career for Flight Simulator</p>
 
     </nav>
   )
